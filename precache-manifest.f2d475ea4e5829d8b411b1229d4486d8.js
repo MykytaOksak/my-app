453fd5488e08280c2a1f0601ccc60419f29043ba
Nikita Oksak <stupidmaadworld@gmail.com>
2020-09-14 23:54:04 +0300

    Updates

---
+++ precache-manifest.f2d475ea4e5829d8b411b1229d4486d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d128139d4cfae92597943eed9d58619",
+    "url": "/MykytaOksak/my-app/index.html"
+  },
+  {
+    "revision": "8b67b2b2cc4516a9da7d",
+    "url": "/MykytaOksak/my-app/static/css/main.05637082.chunk.css"
+  },
+  {
+    "revision": "e636cafdab87407fadb2",
+    "url": "/MykytaOksak/my-app/static/js/2.59f5cbdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MykytaOksak/my-app/static/js/2.59f5cbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b67b2b2cc4516a9da7d",
+    "url": "/MykytaOksak/my-app/static/js/main.b2bb76b5.chunk.js"
+  },
+  {
+    "revision": "dab47276e0de50e1e68b",
+    "url": "/MykytaOksak/my-app/static/js/runtime-main.8eebf1c8.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/MykytaOksak/my-app/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/MykytaOksak/my-app/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/MykytaOksak/my-app/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/MykytaOksak/my-app/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
