2fa506bf4883a1fd03ba7eac2816b234a5ef8ac9
Nikita Oksak <stupidmaadworld@gmail.com>
2020-09-15 00:15:11 +0300

    Updates

---
+++ precache-manifest.049d9a28f9cefe5bdc86350c3dcebc66.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7552c028c5e0946cfdfdc7cade417a40",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2000e7a829433b92a427",
+    "url": "/my-app/static/css/main.0b86e8bf.chunk.css"
+  },
+  {
+    "revision": "4a82cd5f032082b84120",
+    "url": "/my-app/static/js/2.70bf0898.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.70bf0898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2000e7a829433b92a427",
+    "url": "/my-app/static/js/main.fdea04aa.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/my-app/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/my-app/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/my-app/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/my-app/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MykytaOksak/my-app/precache-manifest.44bdad795a1b902754c56b35279b7d12.js"
+  "/my-app/precache-manifest.049d9a28f9cefe5bdc86350c3dcebc66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MykytaOksak/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
